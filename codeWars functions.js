0f4fd39065a2cbb89cd40f13df48d7311cb52381
Anton <elminioelminio@ukr.net>
2021-11-13 18:23:37 +0300

    Create codeWars functions.js

---
+++ codeWars functions.js
@@ -0,0 +1,13 @@
+const checkPalindrome = (number)=>{
+  let digits = number.toString().split('')
+    if(digits.length<=1)
+      return true
+  if(digits.length%2)
+    digits.splice((digits.length-1)/2,1)
+  return digits.slice(0,digits.length/2).every((digit,i)=>digit===digits[digits.length-i-1])
+}
+const reverseNumber = (number)=>{
+  if(number!==number && typeof number !=='number')
+    return 0
+  return parseInt(number.toString().split('').reverse().join(''))
+}
